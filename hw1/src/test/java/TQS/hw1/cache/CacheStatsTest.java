85e53098e6ad0d62ece14a3a494c60edbc73d42c
João <j.morais@ua.pt>
2025-04-09 04:24:45 +0100

    cleaning up

--- hw1/src/test/java/TQS/hw1/cache/CacheStatsTest.java
+++ hw1/src/test/java/TQS/hw1/cache/CacheStatsTest.java
@@ -13,8 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import TQS.hw1.service.WeatherService;
